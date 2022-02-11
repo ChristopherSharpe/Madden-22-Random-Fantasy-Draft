0b4b3176978c2ab55c3ef3f5489dd1781558e91a
ChristopherSharpe <christopher.sharpe@snhu.edu>
2022-02-10 21:32:29 -0600

    Fixed issues
    Fixed issues of randomizedPositions picking duplicates and/or not printing all positions into pickForEachRound Dict.
    All rounds and positions are printing into dict now.

--- randomizer.py
+++ randomizer.py
@@ -2,12 +2,19 @@ import random
 import csv
 
 f = open("MaddenFile.txt", "w")
+#Created the team and position lists
 team_List = ["Cardinals", "Falcons", "Panthers", "Ravens", "Bills", "Bears", "Bengals", "Browns", "Cowboys", "Broncos", "Lions", "Packers", "Texans", "Colts", "Jaguars", "Chiefs", "Raiders", "Chargers", "Rams", "Dolphins", "Vikings", "Patriots", "Saints", "Giants", "Jets", "Eagles", "Steelers", "49ers", "Seahawks", "Buccaneers", "Titans", "Commanders" ]
 position_list = ["QB", "RB", "WR", "FB", "TE", "LT", "RT", "LG", "RG", "C", "DT", "DE", "MLB", "LOLB", "ROLB", "CB", "FS", "SS", "K", "P"]
+
+#Creating dicts to use for number of rounds to be picked, and the number of picks per position, which will be entered by user. 
+numRounds = []
+numPickPerPos = {}
+
+#Creating a function to give users a random team from team list, and writing it to a file, will convert to a csv file soon
 def randomTeamForUsers():
     numOfUsers = int(input ("How many players will there be: "))
     f = open("MaddenFile.txt", "w")
-    
+    #For I in the range of number of users that are entered to play.(User enters a number of players)
     for i in range(numOfUsers):
         userName = (input ("Name of user: "))
         rand_Team = random.choice(team_List)
@@ -17,10 +24,8 @@ def randomTeamForUsers():
         print(team_List)
         f.write(nameAndTeam)
 
-numRounds = []
-numPickPerPos = {}
-newNumPickPerPos = {}
 
+#Creates the function to determine how many rounds to be picked, and how many of each position will be picked. 
 def numOfRounds():
     for x in range(len(position_list)):
         keys = position_list[x]
@@ -33,31 +38,37 @@ def numOfRounds():
     print(sum(numRounds))
     print("You will draft for a total of ", sum(numRounds), " rounds")
 
-
+#Creating the randomizer to randomly give users positions to pick each round. 
 def randomizedPositions():
     pickForEachRound = {}
     rounds = sum(numRounds)
     key = 1
     
-    while rounds > 0:
+    #While rounds doesn't equal 0, this should continue
+    while rounds != 0:
         newKey ="Round: ", key 
+        
+        #Creates a variable for the random position to be chosen from the position list
         posKeys = random.choice(position_list)
-        pickForEachRound[newKey] = posKeys
-        rounds = rounds - 1
-        key = key + 1
-        
-        
         
         for keys, values in numPickPerPos.items():
-            if keys == posKeys:
-                if values != 0:
+            #If the key from the numPickPerPos dict is equal to posKeys(random position chosen from list) and if value from the Key:Value pair in numPickPerPos doesn't equal 0: value - 1 and key + 1 
+            if keys == posKeys and values != 0:
                     numPickPerPos[keys] = values - 1
-                # If the value for random key chosen is 0, return to line 42, or just look for another position with a key whose value > 0
-                elif values == 0:
-                    Do Something  
-                # When all positions have been chosen, break out of loop and contine to next. 
-                else:
-                    break
+                    key = key + 1
+                    rounds = rounds - 1
+                    pickForEachRound[newKey] = posKeys
+            else:
+                continue
+                    
+                        
+    
+             
+        
+        
+            
+                
+    
     print(pickForEachRound)
     print(numPickPerPos)
                   
